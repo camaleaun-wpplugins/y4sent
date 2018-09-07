da56b9d96cc5b3af4a6785f6afb46ad0d35b8060
Gilberto Tavares <camaleaun@gmail.com>
2018-09-07 19:50:42 -0300

    WP-CLI scaffold

---
+++ y4sended.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     WooCommerce Sended Status
+ * Plugin URI:      https://github.com/camaleaun/y4sended
+ * Description:     WooCommerce sended status to order
+ * Author:          Yogh
+ * Author URI:      https://www.yogh.com.br
+ * Text Domain:     y4sended
+ * Domain Path:     /languages
+ * Version:         1.0.0
+ *
+ * @package         Y4sended
+ */
+
+// Your code starts here.
